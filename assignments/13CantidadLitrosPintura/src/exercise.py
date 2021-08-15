984fdb1bf5216eee91e7b6c98db44839422ddeca
stoledol <46824212+stoledol@users.noreply.github.com>
2021-08-15 12:30:28 -0500

    Update exercise.py

--- assignments/13CantidadLitrosPintura/src/exercise.py
+++ assignments/13CantidadLitrosPintura/src/exercise.py
@@ -3,10 +3,10 @@ def main():
     #escribe tu código abajo de esta línea
 
 
-    mts=float(input("Area a pintar en metros: "))
-    rendimiento=float(input("Rendimiento (m2/l): "))
-    litros=math.ceil(mts / rendimiento)
-    print(f"Litros a comprar: {litros}")
+    #mts=float(input("Area a pintar en metros: "))
+    #rendimiento=float(input("Rendimiento (m2/l): "))
+    #litros=
+    #print()
 
 
 if __name__ == '__main__':
