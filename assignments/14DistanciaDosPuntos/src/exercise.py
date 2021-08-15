b1ce696bf052c672bdf8fd7b106135528045a6e0
stoledol <46824212+stoledol@users.noreply.github.com>
2021-08-15 12:33:50 -0500

    Update exercise.py

--- assignments/14DistanciaDosPuntos/src/exercise.py
+++ assignments/14DistanciaDosPuntos/src/exercise.py
@@ -1,14 +1,15 @@
-import math
+# Coloca aquí la librería de matemáticas
+
 def main():
     #escribe tu código abajo de esta línea
     #            
     x1= float(input("x1= "))
-    y1= float(input("y1= "))
-    x2= float(input("x2= "))
-    y2= float(input("y2= "))
+    #y1= 
+    #x2= 
+    #y2= 
 
-    distancia = math.sqrt((x2 - x1) ** 2 + (y2 - y1) ** 2)
-    print (f"distancia= {distancia:.2f}")
-
+    #distancia = 
+    #print ()
+    
 if __name__ == '__main__':
     main()
